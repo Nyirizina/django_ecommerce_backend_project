db3184dd8b8cc8a47cf795ab3732753c00d27e01
Nyirizina <manzi9ivan@gmail.com>
2026-07-10 02:14:17 +0200

    Django web project with home page, login and logout functioning and responsive navigation bar and a regiser account

---
+++ store/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+import datetime 
+
+# categories of products
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+# customers
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.fist_name} {self.last_name}'
+
+# products
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/', default='products/default.jpg')
+
+    #add sales
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return self.name
+
+#customer orders
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=250, default='', blank=True)
+    phone = models.CharField(max_length=30, default='', blank=True)
+    date = models.DateTimeField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
