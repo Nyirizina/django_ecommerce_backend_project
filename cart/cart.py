c2d40aae5eb63989dccf3e4f1412d99e11954d20
Nyirizina <manzi9ivan@gmail.com>
2026-07-16 03:24:21 +0200

    add to cart button functionality and check if the session adhere to the add to cart and keeps the session key for future access

--- cart/cart.py
+++ cart/cart.py
@@ -13,3 +13,14 @@ class Cart():
 
         #make the cart data available to all pages of site
         self.cart = cart
+    
+    def add(self, product):
+        product_id = str(product.id)
+
+        #logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 def cart_summary(request):
     return render(request, 'cart_summary.html')
 
 def cart_add(request):
-    pass
+    # get cart
+    cart = Cart(request)
+    # test for post check if they are posting
+    if request.POST.get('action') == 'post':
+        # what to get after posting
+        product_id = int(request.POST.get('product_id'))
+
+        # look for product in database
+        product = get_object_or_404(Product, id=product_id)
+        # Save to session
+        cart.add(product=product)
+
+        #return response
+        response = JsonResponse({'Product Name: ': product.name})
+        return response
+
+    
 
 def cart_delete(request):
     pass
