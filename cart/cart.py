369bd32c0542aea20fded95f8da8b8948ea47dfc
Nyirizina <manzi9ivan@gmail.com>
2026-07-22 02:22:41 +0200

    adding a total of the cart

--- cart/cart.py
+++ cart/cart.py
@@ -67,6 +67,28 @@ class Cart():
         # delete from dictionary/cart
         if product_id in self.cart:
             del self.cart[product_id]
-            
+
         self.session.modified = True
+    
+    def totals(self):
+        # get product ids
+        product_ids = self.cart.keys()
+        # lookup keys in the products database model
+        products = Product.objects.filter(id__in=product_ids)
+        # get quantities
+        quantities = self.cart
+        # start counting from zero
+        total = 0
+
+        for key,value in quantities.items():
+            key = int(key)
+            for product in products:
+                #will be taking product's value by product_id as key value pairs and multiplying the value by price {"4":2}
+                if product.id == key:
+                    if product.is_sale:
+                       total = total + (product.sale_price * value)
+                    else:
+                       total = total + (product.price * value)
+
+        return total 
 
