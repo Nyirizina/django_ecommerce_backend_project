71639579668b68db68138aa977d578c5607e1ada
Nyirizina <manzi9ivan@gmail.com>
2026-07-23 16:21:24 +0200

    added a profile for user to add more details

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
-from . models import Product, Category, Customer, Order
+from . models import Product, Category, Customer, Order, Profile
+from django.contrib.auth.models import User
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+# mix profile info with user info
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# extend user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email"]
+    inlines = [ProfileInline]
+
+# unregister the previous user details in the admin page
+admin.site.unregister(User)
+
+#Re-register new userdetails
+admin.site.register(User, UserAdmin)
